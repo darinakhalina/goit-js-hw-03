1534b9df9576730ac5898b8c1ca2328e30d69aaf
Darina Khalina <darina.khalina@namecheap.com>
2024-04-26 12:49:14 +0300

    chore(hw-3): add task 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,18 @@
-console.log(3);
+function filterArray(numbers, value) {
+  const filtredArray = [];
+
+  for (const number of numbers) {
+    if (number > value) {
+      filtredArray.push(number);
+    }
+  }
+
+  return filtredArray;
+}
+
+console.log('Task 3');
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
